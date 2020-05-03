baeebd77704e67f39585ff328a5df7a7ad46d793
Aaron Andersen <aaron@fosslib.net>
2020-05-02 21:40:00 -0400

    zerotierone: use systemd to provision state directories

--- nixos/modules/services/networking/zerotierone.nix
+++ nixos/modules/services/networking/zerotierone.nix
@@ -45,18 +45,14 @@ in
 
       path = [ cfg.package ];
 
-      preStart = ''
-        mkdir -p /var/lib/zerotier-one/networks.d
-        chmod 700 /var/lib/zerotier-one
-        chown -R root:root /var/lib/zerotier-one
-      '' + (concatMapStrings (netId: ''
-        touch "/var/lib/zerotier-one/networks.d/${netId}.conf"
-      '') cfg.joinNetworks);
+      preStart = concatMapStrings (netId: ''touch "$STATE_DIRECTORY/networks.d/${netId}.conf"'') cfg.joinNetworks;
       serviceConfig = {
         ExecStart = "${cfg.package}/bin/zerotier-one -p${toString cfg.port}";
         Restart = "always";
         KillMode = "process";
         TimeoutStopSec = 5;
+        StateDirectory = "zerotier-one zerotier-one/networks.d";
+        StateDirectoryMode = "0700";
       };
     };
 
