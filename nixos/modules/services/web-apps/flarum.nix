48c8f0302af71c4db7f271bfe51bb157ed4b2831
Aaron Andersen <aaron@fosslib.net>
2021-03-17 20:02:47 -0400

    nixos/flarum: drop a few symlinks in mutable paths, rework the service

--- nixos/modules/services/web-apps/flarum.nix
+++ nixos/modules/services/web-apps/flarum.nix
@@ -1,74 +1,59 @@
 { config, lib, pkgs, utils, ... }:
 
 let
-  inherit (lib) concatStrings literalExample mapAttrs mkDefault mkEnableOption mkIf mkOption types;
-  inherit (pkgs) writeText;
+  inherit (lib) mkDefault mkEnableOption mkIf mkOption optionalAttrs optionals types;
 
   cfg = config.services.flarum;
 
-  flarumInstallConfig =
-    writeText "config.yml" (builtins.toJSON {
-      debug = false;
-      baseUrl = cfg.baseUrl;
-      databaseConfiguration = cfg.database;
-      adminUser = {
-        username = cfg.adminUser;
-        password = cfg.initialAdminPassword;
-      };
-      settings = {
-        forum_title = cfg.forumTitle;
-      };
-    });
+  # custom variant of https://github.com/flarum/flarum/blob/master/site.php
+  customSite = pkgs.writeText "site.php" ''
+    <?php
 
-  flarum = with pkgs.flarum; pkgs.stdenv.mkDerivation {
-    name = "flarum";
-    preferLocalBuild = true;
-    buildCommand = ''
-      mkdir $out
-      cp -ar ${src}/{site.php,flarum,.nginx.conf} $out/
-      ln -s ${dependencies}/composer.{json,lock} $out/
-      ln -s ${dependencies}/vendor $out/
-      mkdir $out/public
-      cp ${src}/public/index.php $out/public/
-      ln -s ${cfg.stateDir}/assets $out/public/assets
-      ln -s ${cfg.stateDir}/{storage,extensions} $out/
-    '';
+    require '${pkgs.flarum.dependencies}/vendor/autoload.php';
+
+    return Flarum\Foundation\Site::fromPaths([
+        'base' => '${cfg.stateDir}',
+        'public' => '${cfg.stateDir}/public',
+        'storage' => '${cfg.stateDir}/storage',
+        'vendor' => '${pkgs.flarum.dependencies}/vendor',
+    ]);
+  '';
+
+  defaultInstallConfig = lib.generators.toYAML {} {
+    debug = false;
+    baseUrl = "https://${cfg.domain}";
+    databaseConfiguration = {
+      driver = "mysql";
+      host = "localhost";
+      database = "flarum";
+      username = "flarum";
+    };
+    adminUser = {
+      username = "admin";
+      password = "correcthorsebatterystaple";
+      email = "admin@example.net";
+    };
   };
+
+  flarum-cli = pkgs.writeScriptBin "flarum" ''
+    #! ${pkgs.runtimeShell}
+    cd ${cfg.stateDir}
+    sudo=exec
+    if [[ "$USER" != ${cfg.user} ]]; then
+      sudo='exec /run/wrappers/bin/sudo -u ${cfg.user}'
+    fi
+    $sudo ${pkgs.php}/bin/php flarum "$@"
+  '';
 in {
   options = {
     services.flarum = {
       enable = mkEnableOption "Flarum discussion platform";
-
-      forumTitle = mkOption {
-        type = types.str;
-        default = "A Flarum Forum on NixOS";
-        description = "Title of the forum.";
-      };
 
       domain = mkOption {
         type = types.str;
         default = "localhost";
         example = "forum.example.com";
         description = "Domain to serve on.";
-      };
-
-      baseUrl = mkOption {
-        type = types.str;
-        default = "https://${cfg.domain}";
-        example = "https://forum.example.com";
-        description = "Change `domain` instead.";
-      };
-
-      adminUser = mkOption {
-        type = types.str;
-        default = "flarum";
-        description = "Username for first web application administrator";
-      };
-
-      initialAdminPassword = mkOption {
-        type = types.str;
-        default = "flarum";
-        description = "Initial password for the adminUser";
       };
 
       user = mkOption {
@@ -89,120 +74,174 @@ in {
         description = "Home directory for writable storage";
       };
 
-      database = mkOption rec {
-        type = with types; attrsOf (oneOf [str bool int]);
-        description = "MySQL database parameters";
-        default = {
-          # the database driver; i.e. MySQL; MariaDB...
-          driver = "mysql";
-          # the host of the connection; localhost in most cases unless using an external service
-          host = "localhost";
-           # the name of the database in the instance
-          database = "flarum";
-          # database username
-          username = "flarum";
-          # database password
-          password = "";
-          # the prefix for the tables; useful if you are sharing the same database with another service
-          prefix = "";
-          # the port of the connection; defaults to 3306 with MySQL
-          port = 3306;
-          strict = false;
+      database = {
+        createLocally = mkOption {
+          type = types.bool;
+          default = true;
+          description = ''
+            If set to true this option will:
+            - install and configure a MariaDB database on this server
+            - create a database called "flarum" locally
+            - create a database user called ''${services.flarum.user} locally
+            - grant permissions on the flarum database to the locally created user
+          '';
         };
       };
 
-      createDatabaseLocally = mkOption {
-        type = types.bool;
-        default = true;
-        description = "Create the database and database user locally, and run installation.";
+      installConfig = mkOption {
+        type = types.path;
+        description = ''
+          Initial installation configuration to provide Flarum.
+          https://discuss.flarum.org/d/22187-command-line-install-via-php-flarum-install-no-interaction
+        '';
+        default = pkgs.writeText "config.yml" defaultInstallConfig;
+        defaultText = defaultInstallConfig;
+      };
+
+      poolConfig = mkOption {
+        type = with types; attrsOf (oneOf [ str int bool ]);
+        default = {
+          "pm" = "dynamic";
+          "pm.max_children" = 10;
+          "pm.start_servers" = 2;
+          "pm.min_spare_servers" = 1;
+          "pm.max_spare_servers" = 3;
+          "pm.max_requests" = 500;
+        };
+        description = ''
+          Options for the Flarum PHP pool. See the documentation on <literal>php-fpm.conf</literal>
+          for details on configuration directives.
+        '';
       };
     };
   };
 
   config = mkIf cfg.enable {
-    users.users.${cfg.user} = {
-      isSystemUser = true;
-      home = cfg.stateDir;
-      createHome = true;
-      group = cfg.group;
-    };
-    users.groups.${cfg.group} = {};
+
+    # allow the sysadmin to manage database migrations, etc...
+    environment.systemPackages = [ flarum-cli ];
 
     services.phpfpm.pools.flarum = {
-      user = cfg.user;
+      inherit (cfg) user group;
       settings = {
-        "listen.owner" = config.services.nginx.user;
-        "listen.group" = config.services.nginx.group;
+        "listen.owner" = config.services.httpd.user;
+        "listen.group" = config.services.httpd.group;
         "listen.mode" = "0600";
-        "pm" = mkDefault "dynamic";
-        "pm.max_children" = mkDefault 10;
-        "pm.max_requests" = mkDefault 500;
-        "pm.start_servers" = mkDefault 2;
-        "pm.min_spare_servers" = mkDefault 1;
-        "pm.max_spare_servers" = mkDefault 3;
-      };
+      } // cfg.poolConfig;
     };
 
-    services.nginx = {
+    # TODO: enable nginx configuration
+#    services.nginx = {
+#      enable = true;
+#      virtualHosts."${cfg.domain}" = {
+#        root = "${cfg.stateDir}/public";
+#        locations."~ \.php$".extraConfig =
+#          ''
+#          fastcgi_pass unix:${config.services.phpfpm.pools.flarum.socket};
+#          fastcgi_index site.php;
+#          '';
+#        extraConfig =
+#          ''
+#          index index.php;
+#          include ${pkgs.flarum.src}/.nginx.conf;
+#          '';
+#      };
+#    };
+
+    # TODO: delete httpd configuration
+    services.httpd.enable = true;
+    services.httpd.extraModules = [ "proxy_fcgi" ];
+    services.httpd.virtualHosts.${cfg.domain} = {
+      documentRoot = "${cfg.stateDir}/public";
+      extraConfig = ''
+        <Directory ${cfg.stateDir}/public>
+          AllowOverride all
+          DirectoryIndex index.php
+          Require all granted
+          Options FollowSymLinks
+
+          <FilesMatch "\.php$">
+            <If "-f %{REQUEST_FILENAME}">
+              SetHandler "proxy:unix:${config.services.phpfpm.pools.flarum.socket}|fcgi://localhost/"
+            </If>
+          </FilesMatch>
+        </Directory>
+      '';
+    };
+
+    services.mysql = mkIf cfg.database.createLocally {
       enable = true;
-      virtualHosts."${cfg.domain}" = {
-        root = "${cfg.stateDir}/public";
-        locations."~ \.php$".extraConfig =
-          ''
-          fastcgi_pass unix:${config.services.phpfpm.pools.flarum.socket};
-          fastcgi_index site.php;
-          '';
-        extraConfig =
-          ''
-          index index.php;
-          include ${pkgs.flarum.src}/.nginx.conf;
-          '';
-      };
+      package = mkDefault pkgs.mariadb;
+      ensureDatabases = [ "flarum" ];
+      ensureUsers = [
+        { name = cfg.user;
+          ensurePermissions = {
+            "flarum.*" = "ALL PRIVILEGES";
+          };
+        }
+      ];
     };
 
-    services.mysql = mkIf cfg.enable {
-      enable = true;
-      ensureDatabases = [ cfg.database.database ];
-      ensureUsers = [ {
-        name = cfg.database.username;
-        ensurePermissions = {
-          "${cfg.database.database}.*" = "ALL PRIVILEGES";
-        };
-      } ];
-    };
+    systemd.tmpfiles.rules = [
+      # create the required directory structure
+      "d '${cfg.stateDir}' - ${cfg.user} ${cfg.user}"
+      "d '${cfg.stateDir}/public' - ${cfg.user} ${cfg.user}"
+      "d '${cfg.stateDir}/public/assets' - ${cfg.user} ${cfg.user}"
+      "d '${cfg.stateDir}/storage' - ${cfg.user} ${cfg.user}"
+      "d '${cfg.stateDir}/storage/cache' - ${cfg.user} ${cfg.user}"
+      "d '${cfg.stateDir}/storage/formatter' - ${cfg.user} ${cfg.user}"
+      "d '${cfg.stateDir}/storage/less' - ${cfg.user} ${cfg.user}"
+      "d '${cfg.stateDir}/storage/locale' - ${cfg.user} ${cfg.user}"
+      "d '${cfg.stateDir}/storage/logs' - ${cfg.user} ${cfg.user}"
+      "d '${cfg.stateDir}/storage/sessions' - ${cfg.user} ${cfg.user}"
+      "d '${cfg.stateDir}/storage/tmp' - ${cfg.user} ${cfg.user}"
+      "d '${cfg.stateDir}/storage/views' - ${cfg.user} ${cfg.user}"
 
-    assertions = [ {
-      assertion = !cfg.createDatabaseLocally || cfg.database.driver == "mysql";
-      message = "Flarum can only be automatically installed in MySQL/MariaDB.";
-    } ];
+      # unfortunately these few files must exist in a mutable directory
+      "L+ '${cfg.stateDir}/flarum' - - - - ${pkgs.flarum.src}/flarum"
+      "L+ '${cfg.stateDir}/site.php' - - - - ${customSite}"
+      "L+ '${cfg.stateDir}/public/.htaccess' - - - - ${pkgs.flarum.src}/public/.htaccess"
+      "L+ '${cfg.stateDir}/public/index.php' - - - - ${pkgs.flarum.src}/public/index.php"
+
+      # allow the sysadmin to customize flarum
+      "C '${cfg.stateDir}/extend.php' - - - - ${pkgs.flarum.src}/extend.php"
+
+      # ensure ownership if cfg.user or cfg.group has changed
+      "Z '${cfg.stateDir}' - ${cfg.user} ${cfg.user}"
+    ];
 
     systemd.services.flarum-install = {
       description = "Flarum installation";
-      after = [ "mysql.service" ];
-      requires = [ "mysql.service" ];
-      before = [ "nginx.service" "phpfm-flarum.service" ];
-      wantedBy = [ "phpfpm-flarum.service" ];
+      after = optionals cfg.database.createLocally [ "mysql.service" ];
+      wantedBy = [ "multi-user.target" ];
+
+      script = ''
+        ${pkgs.php}/bin/php flarum install --file=${cfg.installConfig}
+        ${pkgs.php}/bin/php flarum migrate
+      '';
+
       serviceConfig = {
         Type = "oneshot";
         User = cfg.user;
         Group = cfg.group;
+        WorkingDirectory = cfg.stateDir;
       };
-      path = [ config.services.phpfpm.phpPackage ];
-      script = ''
-        mkdir -p ${cfg.stateDir}/{extensions,public/assets/avatars}
-        mkdir -p ${cfg.stateDir}/storage/{formatter,sessions,views}
-        cd ${cfg.stateDir}
-        cp -f ${pkgs.flarum.src}/{extend.php,site.php,flarum} .
-        ln -sf ${pkgs.flarum.dependencies}/vendor .
-        ln -sf ${pkgs.flarum.src}/public/index.php public/
-        chmod a+x . public
-      '' + lib.optionalString (cfg.createDatabaseLocally && cfg.database.driver == "mysql") ''
-        if [ ! -f config.php ]; then
-           php flarum install --file=${flarumInstallConfig}
-        fi
-        php flarum migrate
-        php flarum cache:clear
-      '';
+
+      unitConfig = {
+        ConditionPathExists = "!${cfg.stateDir}/config.php";
+      };
+    };
+
+    users.users = optionalAttrs (cfg.user == "flarum") {
+      flarum = {
+        isSystemUser = true;
+        home = cfg.stateDir;
+        group = cfg.group;
+      };
+    };
+
+    users.groups = optionalAttrs (cfg.group == "flarum") {
+      flarum = {};
     };
   };
 }
